c89f9c5400c86d619d5b0721b03aac1cd8f459fd
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2025-02-08 00:41:54 -0800

    image config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["localhost", "127.0.0.1", "static.portadafrente.com"],
+    remotePatterns: [
+      {
+        hostname: "static.portadafrente.com",
+        protocol: "https",
+      },
+    ],
   },
 };
 
